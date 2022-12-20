0decd243ca75cd2e9badca376fc74099d056102b
allen <yushengchen1996@163.com>
2022-12-20 09:46:18 +0800

    Initialize application

---
+++ src/test/java/com/allen/test/springbootcicd/SpringBootCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allen.test.springbootcicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCiCdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
